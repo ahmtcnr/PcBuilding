2166a1452cc93a33b9ed9db17bf7c5f895283c08
Ahmet ÇINAR <32956741+ahmtcnr@users.noreply.github.com>
2021-05-04 16:09:17 +0300

    simple grab object

---
+++ PcBuilding/Assets/Scripts/GrabAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabAnimation : MonoBehaviour
+{
+
+    public Animator handOpenClose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handOpenClose = GetComponent<Animator>();
+    }
+
+
+    public void openHand(){
+        
+        handOpenClose.SetBool("Open",true);
+        Debug.Log("yo ?");
+
+    }
+    public void closeHand(){
+        handOpenClose.SetBool("Open",false);
+        
+    }
+}
---
+++ PcBuilding/Assets/Scripts/PickUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public Transform theDestination;
+    public static GrabAnimation grabInstance;
+    void Start(){
+
+        grabInstance = GameObject.Find("acilan").GetComponent<GrabAnimation>();
+        
+    }
+
+
+
+    IEnumerator waiter(){
+        grabInstance.openHand();
+        yield return new WaitForSeconds(1);
+        
+        
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDestination.position;
+        this.transform.parent = GameObject.Find("Grab").transform;
+    }
+
+    void OnMouseDown(){
+        StartCoroutine(waiter());
+        
+        
+        
+    }
+
+    void OnMouseUp(){
+
+        this.transform.parent=null;
+        GetComponent<Rigidbody>().useGravity = true;
+        grabInstance.closeHand();
+        
+    }
+}
