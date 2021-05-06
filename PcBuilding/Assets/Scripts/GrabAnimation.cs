236e6e9c4bc9d776a1ee2c3abd563b0993d00547
Ahmet ÇINAR <32956741+ahmtcnr@users.noreply.github.com>
2021-05-06 09:08:52 +0300

    add slower hand position changing

--- PcBuilding/Assets/Scripts/GrabAnimation.cs
+++ PcBuilding/Assets/Scripts/GrabAnimation.cs
@@ -6,21 +6,49 @@ public class GrabAnimation : MonoBehaviour
 {
 
     public Animator handOpenClose;
+
+    bool limiter = false;
+    float animTimer=0f;
+    Vector3 pos1;
     // Start is called before the first frame update
     void Start()
     {
         handOpenClose = GetComponent<Animator>();
     }
 
+    void Update(){
+
+        if(limiter){
+        transform.position =  Vector3.MoveTowards(transform.position,pos1,0.01f);;
+        animTimer+=Time.deltaTime;
+
+            if (animTimer > 1f)
+            {
+                limiter = false;
+            }
+        }
+    }
 
     public void openHand(){
         
         handOpenClose.SetBool("Open",true);
-        Debug.Log("yo ?");
+        
 
     }
     public void closeHand(){
         handOpenClose.SetBool("Open",false);
         
+    }
+
+
+    public void handToObject(Vector3 pos){
+
+        
+        Debug.Log(pos);
+        pos1 = pos;
+        limiter = true;
+
+        
+
     }
 }
--- PcBuilding/Assets/Scripts/PickUp.cs
+++ PcBuilding/Assets/Scripts/PickUp.cs
@@ -7,6 +7,11 @@ public class PickUp : MonoBehaviour
 
     public Transform theDestination;
     public static GrabAnimation grabInstance;
+
+     
+
+    public float edgeRight1;
+    public float edgeUp;    
     void Start(){
 
         grabInstance = GameObject.Find("acilan").GetComponent<GrabAnimation>();
@@ -17,12 +22,20 @@ public class PickUp : MonoBehaviour
 
     IEnumerator waiter(){
         grabInstance.openHand();
+        edgeRight1 = this.transform.position.x + (this.transform.localScale.x / 2f); 
+        edgeUp = this.transform.position.y + (this.transform.localScale.y / 2f); 
+        Debug.Log(edgeRight1);
+        Vector3 pos = new Vector3(edgeRight1+0.1f,edgeUp,transform.position.z);
+        grabInstance.handToObject(pos);
+
         yield return new WaitForSeconds(1);
         
         
         GetComponent<Rigidbody>().useGravity = false;
         this.transform.position = theDestination.position;
         this.transform.parent = GameObject.Find("Grab").transform;
+        
+        
     }
 
     void OnMouseDown(){
